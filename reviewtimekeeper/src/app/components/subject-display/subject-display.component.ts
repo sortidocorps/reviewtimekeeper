c793deb79b92eacc95374991385ee5a217b847fc
sagcloud <sagcloud@yopmail.com>
2020-03-02 16:24:13 +0100

    color change time left

--- reviewtimekeeper/src/app/components/subject-display/subject-display.component.ts
+++ reviewtimekeeper/src/app/components/subject-display/subject-display.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SubjectcompoComponent } from '../subjectcompo/subjectcompo.component';
+import { isGeneratedFile } from '@angular/compiler/src/aot/util';
 
 @Component({
   selector: 'app-subject-display',
@@ -9,11 +10,99 @@ import { SubjectcompoComponent } from '../subjectcompo/subjectcompo.component';
 })
 export class SubjectDisplayComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<SubjectcompoComponent>,@Inject(MAT_DIALOG_DATA) public data: any) { }
+  time25: number;
+  time5: number;
+  interval;
+  timeStarted: number;
+  timeLeft: number;
+  colorBg: string;
+  isPaused: boolean;
+
+  constructor(public dialogRef: MatDialogRef<SubjectcompoComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.timeLeft = this.data.timeLeft;
+    this.isPaused = false;
+  }
 
 
 
   ngOnInit(): void {
+    this.startTimer();
+    this.colorBg = "good";
+
+    this.timeStarted = this.timeLeft;
+
+    let percent = 25;
+    this.time25 = parseInt("" + Math.round(this.timeStarted * percent) / 100);
+
+    percent = 5;
+    this.time5 = parseInt("" + Math.round(this.timeStarted * percent) / 100);
+
+    console.log("time25 ", this.time25);
+    console.log("time5 ", this.time5);
   }
+
+  startTimer() {
+    this.isPaused = false;
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        clearInterval(this.interval);
+        this.closerPopup();
+      }
+      this.lastButNotLeast();
+
+    }, 1000)
+
+
+  }
+
+  closerPopup() {
+    if (this.timeLeft <= 0) {
+
+      // Close the popup 2 seconds after the end of the subject
+      let timeToClosePopup = 2;
+      let closePopupInterval = setInterval(() => {
+        if (timeToClosePopup > 0) {
+          timeToClosePopup--;
+        } else {
+          clearInterval(closePopupInterval);
+        }
+        // The time is realy finish.
+        this.dialogRef.close("ok");
+
+      }, 1000)
+
+    }
+  }
+
+  pauseTimer() {
+    if (this.isPaused) {
+      this.startTimer();
+    } else {
+      clearInterval(this.interval);
+      this.isPaused = true;
+    }
+
+  }
+
+  lastButNotLeast() {
+
+    if (this.timeLeft == this.time25) {
+      this.colorBg = "warning";
+    }
+
+    if (this.timeLeft == this.time5) {
+      this.colorBg = "error";
+    }
+
+
+  }
+
+
+  closeDial() {
+    this.dialogRef.close("");
+  }
+  
 
 }
--- reviewtimekeeper/src/app/components/subjectcompo/subjectcompo.component.ts
+++ reviewtimekeeper/src/app/components/subjectcompo/subjectcompo.component.ts
@@ -10,86 +10,69 @@ import { SubjectDisplayComponent } from '../subject-display/subject-display.comp
   styleUrls: ['./subjectcompo.component.scss']
 })
 export class SubjectcompoComponent implements OnInit {
- 
 
-  public title:string = "";
-  public who:string = "";
-  public order:number = 0;
-  public timeLeft:number = 60;
-  public timeStarted:number;
+
+  public title: string = "";
+  public who: string = "";
+  public order: number = 0;
+  public timeLeft: number = 60;
+  public timeStarted: number;
+
+  public colorForms:string;
 
   interval;
 
-  isDel:boolean = false;
-  time25:number;
-  time5:number;
+  isDel: boolean = false;
+  istOk: boolean = false;
+  time25: number;
+  time5: number;
 
   constructor(public dialog: MatDialog) {
-   }
+  }
 
   ngOnInit(): void {
   }
 
-  startTimer() {    
-    this.timeStarted = this.timeLeft;
-    console.log("this.timeStarted ",this.timeStarted);
-
-    let percent = 25;
-    this.time25 = Math.round(this.timeStarted*percent) / 100;
-    console.log(this.time25);
-    percent = 5;
-    this.time5 = Math.round(this.timeStarted*percent) / 100;
-    console.log(this.time5);
-
+  startTimer() {
 
     const dial = this.dialog.open(SubjectDisplayComponent, {
+      position: {
+        top: '25px',
+        /* right: '10px' */
+      },
+      disableClose: true,
+      height: '100vw',
+      width: '100vw',
+      panelClass: 'myapp-no-padding-dialog',
       data: {
-        title: "IDF ",
-        idfIndus: "element.idfIndus",
-        idfProd: "element.idfProd"
+        title: this.title,
+        who: this.who,
+        timeLeft: this.timeLeft
       }
     });
 
-    this.interval = setInterval(() => {
-      if(this.timeLeft > 0) {
-        this.timeLeft--;
+    dial.afterClosed().subscribe(result => {
+      if(result == "ok") {
+        this.colorForms = "good";
       } else {
-        this.timeLeft = 60;
+        this.colorForms = "warning";
       }
-      this.lastButNotLeast();
+    });
 
-    },1000)
-  }
-
-  pauseTimer() {
-    clearInterval(this.interval);
   }
 
 
   deleteItem() {
-    
+
     this.isDel = true;
 
   }
 
-  isDelete(){
+  isDelete() {
     return this.isDel;
   }
 
-  lastButNotLeast() {
-    
-    if (this.timeLeft == this.time25) {
-      console.log("YEAHHHHHHHHHH ", this.time25);
-    }
-
-    if (this.timeLeft == this.time5) {
-      console.log("YEAHHHHHHHHHH5 ", this.time5);
-    }
 
 
-  }
-
-  
-  
 
 }
