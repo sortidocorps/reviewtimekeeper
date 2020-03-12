8b3a9512177f1621d1288e6831a34f49a8b4810a
E000421 <florian.martin_ext@michelin.com>
2020-03-12 13:23:58 +0100

    change tab subject

--- reviewtimekeeper/src/app/components/subjectcompo/subjectcompo.component.ts
+++ reviewtimekeeper/src/app/components/subjectcompo/subjectcompo.component.ts
@@ -1,8 +1,17 @@
+import { ReviewSubject } from './../../entities/reviewSubject';
 import { Component, OnInit, ComponentRef, ViewContainerRef } from '@angular/core';
-import { Subject } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { SubjectDisplayComponent } from '../subject-display/subject-display.component';
+import { MatTableDataSource } from '@angular/material/table';
 
+const aSubject = new ReviewSubject();
+aSubject.time = 100;
+aSubject.title = 'Hello World';
+aSubject.who = 'Toto';
+
+const SUBJECTS = [
+  aSubject
+];
 
 @Component({
   selector: 'app-subjectcompo',
@@ -11,6 +20,16 @@ import { SubjectDisplayComponent } from '../subject-display/subject-display.comp
 })
 export class SubjectcompoComponent implements OnInit {
 
+  public subject: ReviewSubject[] = [];
+  dataSource: MatTableDataSource<ReviewSubject>;
+
+  displayColumns: string[] = [
+    'title',
+    'who',
+    'time',
+    'create',
+    'delete'
+  ];
 
   public title: string = "";
   public who: string = "";
@@ -19,7 +38,7 @@ export class SubjectcompoComponent implements OnInit {
   public timeLeft: number;
   public timeStarted: number;
 
-  public colorForms:string;
+  public colorForms: string;
 
   interval;
 
@@ -28,13 +47,18 @@ export class SubjectcompoComponent implements OnInit {
   time25: number;
   time5: number;
 
-  constructor(public dialog: MatDialog) {
+  constructor(
+    public dialog: MatDialog
+  ) {
   }
 
   ngOnInit(): void {
+    this.subject.push(aSubject);
+    console.log(this.subject);
+    this.dataSource = new MatTableDataSource(this.subject);
   }
 
-  startTimer() {
+  startTimer(row: ReviewSubject) {
 
     this.timeLeft = this.timeMinute * 60;
 
@@ -48,23 +72,25 @@ export class SubjectcompoComponent implements OnInit {
       height: '100%',
       width: '100%',
       panelClass: 'myapp-no-padding-dialog',
-      data: {
-        title: this.title,
-        who: this.who,
-        timeLeft: this.timeLeft
-      }
+      data: this.subject[this.subject.indexOf(row)]
     });
-
     dial.afterClosed().subscribe(result => {
-      if(result == "ok") {
+      if (result == "ok") {
         this.colorForms = "good";
       } else {
         this.colorForms = "warning";
       }
     });
-
   }
 
+  addSubject() {
+    let newSubject = new ReviewSubject();
+    newSubject.title = '';
+    newSubject.who = '';
+    newSubject.time = 0;
+    this.subject.push(newSubject);
+    this.dataSource.data = this.subject;
+  }
 
   deleteItem() {
 
@@ -75,8 +101,4 @@ export class SubjectcompoComponent implements OnInit {
   isDelete() {
     return this.isDel;
   }
-
-
-
-
 }
